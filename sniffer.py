78c7ca3c4977469dd2554762c01849c0f30c1c02
Marvin Tetteh <131989527+ScorpionLordbot@users.noreply.github.com>
2024-08-17 04:04:33 -0400

    2

--- sniffer.py
+++ sniffer.py
@@ -4,5 +4,5 @@ def sniff_packets(interface):
   scapy.sniff(iface=interface, store=False, prn=lambda x: x.summary())
 
 if __name__ == "__main__":
-  interface = "your_interface"  # Replace with your network interface
+  interface = "eth0"  # Replace with your network interface
   sniff_packets(interface)
