dcec1122554d4bb43f91d81d38343b52ec4940ec
Marvin Tetteh <mar18teh@gmail.com>
2024-08-17 04:39:25 -0400

    2

--- sniffer.py
+++ sniffer.py
@@ -1,8 +1,17 @@
 import scapy.all as scapy
 
 def sniff_packets(interface):
-  scapy.sniff(iface=interface, store=False, prn=lambda x: x.summary())
+    def packet_handler(packet):
+        if IP in packet:
+            src_ip = packet[IP].src
+            dst_ip = packet[IP].dst
+            protocol = packet[IP].proto
+            print(f"Source: {src_ip}, Destination: {dst_ip}, Protocol: {protocol}")
+        else:
+            print("Non-IP packet")
+
+    scapy.sniff(iface=interface, store=False, prn=packet_handler)
 
 if __name__ == "__main__":
-  interface = "eth0"  # Replace with your network interface
-  sniff_packets(interface)
+    interface = "eth0"
+    sniff_packets(interface)
